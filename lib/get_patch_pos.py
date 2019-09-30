f03e91faabcc7d2ffc298286e73bfd7ffa1ab79b
Kristoffer Roise <kristoffer.roise@outlook.com>
2019-09-30 17:16:19 +0200

    Credits added

--- lib/get_patch_pos.py
+++ lib/get_patch_pos.py
@@ -1,3 +1,7 @@
+# Yang, X., Kwitt, R., Styner, M., Niethammer, M., 2017.
+# Quicksilver: Fast predictive imageregistration - a deep learning approach.
+# arXiv:1703.10908.
+
 import torch
 import numpy as np
 import h5py
