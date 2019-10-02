087914ca601bacd4aa12fc8aacb195f0a7fdef96
Kristoffer Roise <kristoffer.roise@outlook.com>
2019-10-02 16:03:37 +0200

    Created code for running network

--- temp_main.py
+++ temp_main.py
@@ -1,6 +1,10 @@
 from lib.HDF5Image import load_hdf5
 import lib.utils as ut
 from lib.patch_volume import create_patches
+import lib.network as network
+import torch
+import torch.optim as optim
+import torch.nn as nn
 
 fixed_file = 'J249J70K_proc.h5'
 moving_file = 'J249J70M_proc.h5'
@@ -14,4 +18,18 @@ patch_size = 20
 
 input_batch = create_patches(data_norm, patch_size, stride)
 
-ut.show_single(input_batch[31, :], input_batch[565, :].shape)
+print(input_batch[0, 1:].shape)
+#ut.show_single(input_batch[234, 1:], input_batch[234, 1:].shape)
+
+print(input_batch[234:235, 1:].shape)
+
+net = network.Net()
+print(net)
+
+# input = torch.randn(1, 1, 20, 20, 20)  # Random input
+input = input_batch[237:240, 1:]  # Real data input
+out = net(input)
+print(out)
+
+#criterion = nn.CrossEntropyLoss()
+#optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
