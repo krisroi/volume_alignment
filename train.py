7755effd3c8b07315b8fa139888f57bfa60cefad
Kristoffer Roise <kristoffer.roise@outlook.com>
2019-10-22 17:01:38 +0200

    Created a (non-functional) training algorithm

--- train.py
+++ train.py
@@ -1,59 +1,88 @@
 import torch
 import torch.optim as optim
 import lib.network as network
-import lib.affine as affine
-import cv2
-from lib.HDF5Image import load_hdf5
+import lib.affine as A
+from lib.HDF5Image import HDF5Image
 from lib.patch_volume import create_patches
 import numpy as np
+from lib.ncc_loss import NCC
+import lib.utils as ut
 
-# Workflow
 
-
-def train_network(input_batch, image):
+def train_network(fixed_image, warped_image, learning_rate, epochs):
     net = network.Net()
-    optimizer = optim.Adam(net.parameters(), lr=0.01)
+    net.train()
 
-    optimizer.zero_grad()
+    criterion = NCC()
+    optimizer = optim.Adam(net.parameters(), lr=learning_rate)
+    print('Fixed shape: {} \t Warped shape: {}'.format(fixed_image.shape, warped_image.shape))
 
-    # Forward pass
-    pred_theta = net(input_batch)
+    N = int(fixed_image.shape[0] / 32)
 
-    predicted_image = affine.affine_transform(image, pred_theta)
+    for epoch in range(epochs):
 
-    '''
-    Now you should calculate some loss connected with the predicted image and backpropagate that loss into the system.
+        for idx in range(fixed_image.shape[0] - 64):
 
-    loss.backward()
-    loss_value = loss.item()
+            optimizer.zero_grad()  # Zeroing the gradients
 
-    optimizer.step()
-    '''
+            # Perform forward pass on moving pathces
+            predicted_theta = net(warped_image[idx:idx + 63, :])
+            predicted_deform = A.affine_transform(warped_image, predicted_theta, patch=True)
 
-    return predicted_image
+            #print('Predicted deformation shape: ', predicted_deform.shape)
+            #ut.show_single(predicted_deform[0, :].detach().numpy(), predicted_deform[0, :].shape)
+            #ut.show_single(fixed[patch, :].unsqueeze(0), fixed.shape)
+
+            loss = criterion(fixed_image[idx, :].unsqueeze(0),
+                             predicted_deform[idx, :].unsqueeze(0), patch=True)
+            loss.backward()
+            loss_val = loss.item()
+
+            optimizer.step()
+
+            if idx % 50 == 0:
+                print('====> Epoch: {} #\t Loss: {}'.format(epoch, loss_val))
+
+    # return predicted_theta
 
 
 if __name__ == '__main__':
 
-    fixed_file = 'J249J70K_proc.h5'
-    moving_file = 'J249J70M_proc.h5'
+    # Defining filepath
+    PROJ_ROOT = '/users/kristofferroise/project'
+    patient_group = 'patient_data/gr5_STolav5to8'
+    patient = 'p7_3d'
+    fixfile = 'J249J70K_proc.h5'
+    movfile = 'J249J70M_proc.h5'
+    fixvol_no = 'vol01'
+    movvol_no = 'vol02'
 
-    data, shape = load_hdf5(fixed_file, moving_file)
-    image = data.unsqueeze(1)
+    # Loading volumetric data
+    vol_data = HDF5Image(PROJ_ROOT, patient_group, patient,
+                         fixfile, movfile,
+                         fixvol_no, movvol_no)
 
-    stride = 20
     patch_size = 20
+    stride = 20
 
-    input_batch = create_patches(data, patch_size, stride)
-    print(input_batch[0:128, 1:].shape)
-    input_batch = input_batch[0:128, 1:]
+    patched_data = create_patches(vol_data.data, patch_size, stride)
 
-    warped_image = train_network(input_batch, image)
+    # Creating known-transformation moving data
+    theta_trans = torch.FloatTensor([[[0.98, 0.02, 0, 0.02],
+                                      [0.02, 1, 0, 0.02],
+                                      [0, 0, 1, 0.02]]])
 
-    warped_image = np.array(warped_image.detach().numpy(), dtype=np.uint8)
-    print("Warped image shape: " + str(warped_image.shape))
+    theta_idt = torch.FloatTensor([[[1, 0, 0, 0],
+                                    [0, 1, 0, 0],
+                                    [0, 0, 1, 0]]])
 
-    cv2.imshow('x', warped_image[0, 0, int(warped_image.shape[2] / 2), :, :])
+    warped_image = A.affine_transform(patched_data, theta_trans, patch=True)
+    fixed_image = patched_data[:, 0, :]
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    #=======================PARAMETERS==========================#
+    lr = 0.001  # learning rate
+    epochs = 1  # number of epochs
+    #===========================================================#
+
+    predicted_theta = train_network(fixed_image, warped_image, lr, epochs)
+    print(predicted_theta)
