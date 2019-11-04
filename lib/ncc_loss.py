1a548d58a2a324745e9dde6f160fface639b1ae2
Kristoffer Roise <kristoffer.roise@outlook.com>
2019-11-04 17:00:16 +0100

    Added reduction option

--- lib/ncc_loss.py
+++ lib/ncc_loss.py
@@ -6,19 +6,20 @@ class NCC(nn.Module):
     def __init__(self):
         super(NCC, self).__init__()
 
-    def forward(self, fixed_image, warped_image):
+    def forward(self, fixed_image, warped_image, reduction):
         # Creates a forward pass for the loss function
-        return normalized_cross_correlation(fixed_image, warped_image)
+        return normalized_cross_correlation(fixed_image, warped_image, reduction)
 
 
-def normalized_cross_correlation(fixed_image, warped_image):
+def normalized_cross_correlation(fixed_image, warped_image, reduction='mean'):
     """ Defines a loss after comparing two volumes
     Args:
         warped_image: the transformed image with shape [B, C, D, H, W]
         fixed_image: the image in which to compare to the transformed
             image with shape [B, C, D, H, W]
+        reduction: how to reduce the loss-tensor. Default: 'mean' (opt: 'mean', 'sum')
     Returns:
-        a floating tensor of number(s) as defined by either 1 - ncc or -ncc
+        a reduced single float tensor as defined by either 1 - ncc or -ncc
         (defined in the return statement)
     """
 
@@ -33,5 +34,10 @@ def normalized_cross_correlation(fixed_image, warped_image):
 
     alpha = 1.0e-16  # small number to prevent zero-division
     ncc = torch.div(num, (den + alpha))
+
+    if reduction == 'mean':
+        ncc = torch.mean(ncc, dim=0, keepdim=True)
+    elif reduction == 'sum':
+        ncc = torch.sum(ncc, dim=0, keepdim=True)
 
     return 1 - ncc  # Try experimenting with 1 - NCC and -NCC
