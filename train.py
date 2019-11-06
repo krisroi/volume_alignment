ccea8e7f581fb0ab95a8411aa16bd3eb3ec37c0e
Kristoffer Roise <kristoffer.roise@outlook.com>
2019-11-06 18:20:14 +0100

    Changed to use Pytorch's DataLoader

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import torch
+from torch.utils.data import Dataset, DataLoader
 import torch.optim as optim
 import numpy as np
 import math
@@ -13,46 +14,97 @@ from lib.HDF5Image import HDF5Image
 from lib.patch_volume import create_patches
 from lib.ncc_loss import NCC
 import lib.utils as ut
-from lib.dataloader import Dataset
+from lib.dataloader import GetDatasetInformation
+
+
+class CreateDataset(Dataset):
+    def __init__(self, fixed_patches, moving_patches):
+        self.fixed_patches = fixed_patches
+        self.moving_patches = moving_patches
+
+    def __len__(self):
+        return self.fixed_patches.shape[0]
+
+    def __getitem__(self, idx):
+        sample = {'fixed_patches': self.fixed_patches[idx, :],
+                  'moving_patches': self.moving_patches[idx, :]}
+        return sample
+
+
+def validate(path_to_files, val_fix_set, val_mov_set, val_fix_vols, val_mov_vols, batch_size, net, criterion, device):
+
+    for val_set_idx in range(len(val_fix_set)):
+
+        validation_data = HDF5Image(path_to_files, val_fix_set[val_set_idx], val_mov_set[val_set_idx],
+                                    val_fix_vols[val_set_idx], val_mov_vols[val_set_idx])
+        validation_data.normalize()
+        validation_data.to(device)
+
+        patched_validation_data, _ = create_patches(validation_data.data, patch_size, stride, device, voxelsize)
+
+        fixed_patches = patched_validation_data[:, 0, :].unsqueeze(1).to(device)
+        moving_patches = patched_validation_data[:, 1, :].unsqueeze(1).to(device)
+
+        validation_set = CreateDataset(fixed_patches, moving_patches)
+        validation_loader = DataLoader(validation_set, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True)
+
+        val_set_loss = torch.zeros(int(fixed_patches.shape[0] / batch_size) + 1, device=device)
+        validation_loss = torch.zeros(num_validation_sets, device=device)
+
+        for batch_idx, val_data in enumerate(validation_loader):
+
+            batched_fixed_patches = val_data['fixed_patches']
+            batched_moving_patches = val_data['moving_patches']
+
+            predicted_theta = net(batched_moving_patches)
+
+            predicted_deform = A.affine_transform(batched_moving_patches, predicted_theta)
+
+            loss = criterion(batched_fixed_patches, predicted_deform, reduction='mean')
+
+            val_set_loss[batch_idx] = loss.item()
+
+            if batch_idx % 1 == 0:
+                print('====> Validating ... \t Datapart: {}/{} \t Batch: {}/{}'  # \t Remaining time: Calculating ...'
+                      .format(val_set_idx + 1, num_validation_sets, batch_idx + 1, int(fixed_patches.shape[0] / batch_size) + 1))
+
+        validation_loss[val_set_idx] = torch.mean(val_set_loss)
+
+    return torch.mean(validation_loss)
 
 
 def train_data(patched_data, epoch, epochs, lr, batch_size, net, criterion,
-               optimizer, set_idx, num_sets, device):
+               optimizer, set_idx, num_sets, device, num_validation_sets):
 
     fixed_patches = patched_data[:, 0, :].unsqueeze(1).to(device)
     moving_patches = patched_data[:, 1, :].unsqueeze(1).to(device)
 
-    patch_loss = torch.zeros(fixed_patches.shape[0], device=device)
+    train_set = CreateDataset(fixed_patches, moving_patches)
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
 
-    for idx in range(fixed_patches.shape[0]):
+    patch_loss = torch.zeros(int(fixed_patches.shape[0] / batch_size) + 1, device=device)
 
-        optimizer.zero_grad()  # Zeroing the gradients
+    for batch_idx, train_data in enumerate(train_loader):
+        batched_fixed_patches = train_data['fixed_patches']
+        batched_moving_patches = train_data['moving_patches']
 
-        if ((idx + batch_size) > fixed_patches.shape[0]):
-            idx_arr = torch.LongTensor(range((idx - batch_size), idx))
+        optimizer.zero_grad()
 
-        elif ((idx - batch_size) < 0):
-            idx_arr = torch.LongTensor(range(idx, (idx + batch_size)))
+        predicted_theta = net(batched_moving_patches)
 
-        else:
-            idx_arr = torch.LongTensor(range((idx - int(batch_size / 2)), (idx + int(batch_size / 2))))
+        predicted_deform = A.affine_transform(batched_moving_patches, predicted_theta)
 
-        predicted_theta = net(moving_patches[idx_arr, :])
-
-        predicted_deform = A.affine_transform(moving_patches[idx_arr, :],
-                                              predicted_theta)
-
-        loss = criterion(fixed_patches[idx_arr, :],
-                         predicted_deform, reduction='mean')
+        loss = criterion(batched_fixed_patches, predicted_deform, reduction='mean')
 
         loss.backward()
-        patch_loss[idx] = loss.item()
+        patch_loss[batch_idx] = loss.item()
+        print(loss.item())
 
         optimizer.step()
 
-        if idx % 30 == 0:
-            print('====> Epoch: {}/{} \t Datapart: {}/{} \t Patch: {}/{}'  # \t Remaining time: Calculating ...'
-                  .format(epoch + 1, epochs, set_idx + 1, num_sets, idx, fixed_patches.shape[0]))
+        if batch_idx % 1 == 0:
+            print('====> Epoch: {}/{} \t Datapart: {}/{} \t Batch: {}/{}'  # \t Remaining time: Calculating ...'
+                  .format(epoch + 1, epochs, set_idx + 1, num_sets - num_validation_sets, batch_idx + 1, int(fixed_patches.shape[0] / batch_size) + 1))
 
         cur_state_dict = net.state_dict()
 
@@ -60,13 +112,7 @@ def train_data(patched_data, epoch, epochs, lr, batch_size, net, criterion,
 
 
 def train_network(path_to_files, fix_set, mov_set, epochs, lr, batch_size, patch_size,
-                  stride, fix_vols, mov_vols, loss_path, num_sets, device, model_name, voxelsize):
-    '''with open(loss_path, 'w') as els:
-        fieldnames = ['epoch', 'epoch_loss', 'lr=' + str(lr), 'batch_size=' + str(batch_size),
-                      'patch_size=' + str(patch_size), 'stride=' + str(stride),
-                      'number_of_datasets=' + str(num_sets), 'device=' + str(device)]
-        epoch_writer = csv.DictWriter(els, fieldnames=fieldnames)
-        epoch_writer.writeheader()'''
+                  stride, fix_vols, mov_vols, loss_path, num_sets, device, model_name, voxelsize, num_validation_sets):
 
     net = Net().to(device)
     net.train()
@@ -74,7 +120,7 @@ def train_network(path_to_files, fix_set, mov_set, epochs, lr, batch_size, patch
     criterion = NCC().to(device)
     optimizer = optim.Adam(net.parameters(), lr=lr)
 
-    temp_loss = torch.zeros(len(fix_set)).to(device)
+    set_loss = torch.zeros(tot_num_sets - num_validation_sets).to(device)
     epoch_loss = torch.zeros(epochs).to(device)
 
     for epoch in range(epochs):
@@ -86,23 +132,24 @@ def train_network(path_to_files, fix_set, mov_set, epochs, lr, batch_size, patch
         #print('Current memory allocated: ', (((torch.cuda.memory_allocated(device=device)/1024)/1024)/1024))
         #print('Current memory cached: ', (((torch.cuda.memory_cached(device=device)/1024)/1024)/1024))
 
-        for set_idx in range(num_sets):
+        fix_set, mov_set, fix_vols, mov_vols = shuffle(fix_set, mov_set, fix_vols, mov_vols)
+
+        # Defining validation set
+        val_fix_set = fix_set[num_sets - num_validation_sets:num_sets]
+        val_mov_set = mov_set[num_sets - num_validation_sets:num_sets]
+        val_fix_vols = fix_vols[num_sets - num_validation_sets:num_sets]
+        val_mov_vols = mov_vols[num_sets - num_validation_sets:num_sets]
+
+        for set_idx in range(num_sets - num_validation_sets):
 
             print('Loading next set of volume data ...')
             vol_data = HDF5Image(path_to_files, fix_set[set_idx], mov_set[set_idx],
                                  fix_vols[set_idx], mov_vols[set_idx])
             vol_data.normalize()
+            vol_data.to(device)
 
             print('Patching loaded data ...')
-            patched_data, loc = create_patches(vol_data.data, patch_size, stride, device, voxelsize)
-            patch_location = 'output/txtfiles/patch_location.csv'
-
-            with open(patch_location, 'w') as lctn:
-                fieldnames = ['x_pos', 'y_pos', 'z_pos']
-                field_writer = csv.DictWriter(lctn, fieldnames=fieldnames)
-                field_writer.writeheader()
-                lctn_writer = csv.writer(lctn, delimiter=',')
-                lctn_writer.writerows(loc.cpu().numpy().round(5))
+            patched_data, _ = create_patches(vol_data.data, patch_size, stride, device, voxelsize)
 
             print('Training on the loaded data ...')
             training_loss, cur_state_dict = train_data(patched_data,
@@ -116,20 +163,26 @@ def train_network(path_to_files, fix_set, mov_set, epochs, lr, batch_size, patch
                                                        set_idx,
                                                        num_sets,
                                                        device,
+                                                       num_validation_sets
                                                        )
 
-            temp_loss[set_idx] = torch.mean(training_loss)
+            set_loss[set_idx] = torch.mean(training_loss)
 
-        '''model_info = {'patch_size': patch_size,
+        model_info = {'patch_size': patch_size,
                       'state_dict': cur_state_dict}
         print('Saving model ... ')
-        torch.save(model_info, model_name)'''
+        torch.save(model_info, model_name)
 
-        epoch_loss[epoch] = torch.mean(temp_loss)
+        print('Validating at the end of epoch {}'.format(epoch + 1))
+        with torch.no_grad():
+            validation_loss = validate(path_to_files, val_fix_set, val_mov_set, val_fix_vols, val_mov_vols,
+                                       batch_size, net.eval(), criterion, device)
 
-        '''with open(loss_path, mode='a') as epoch_file:
-            epoch_writer = csv.writer(epoch_file, delimiter=',')
-            epoch_writer.writerow([(epoch + 1), epoch_loss[epoch]])'''
+        epoch_loss = torch.mean(set_loss)
+
+        with open(loss_path, mode='a') as loss:
+            loss_writer = csv.writer(loss, delimiter=',')
+            loss_writer.writerow([(epoch + 1), epoch_loss.item(), validation_loss.item()])
 
     return epoch_loss
 
@@ -139,44 +192,46 @@ if __name__ == '__main__':
     #=======================PARAMETERS==========================#
     lr = 1e-4  # learning rate
     epochs = 1  # number of epochs
-    use_sets = 26  # number of datasets that we wish to use. Max of 26
-    shuffle_sets = True
+    tot_num_sets = 4  # Total number of sets to use
+    num_validation_sets = 1
     batch_size = 16
     patch_size = 30
-    stride = 30
+    stride = 29
     voxelsize = 7.0000003e-4
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     #===========================================================#
 
     #=======================SAVING DATA=========================#
-    train = True
     now = datetime.now()
     date = now.strftime('%d%m%Y')
     time = now.strftime('%H%M%S')
-    model_name = 'output/models/model_{}_{}.pt'.format(date, time)
-    loss_path = 'output/txtfiles/loss_{}_epochs_{}_{}.csv'.format(epochs, date, time)
-    #===========================================================#
+    model_name = 'output/discard/model_{}_{}.pt'.format(date, time)
+    loss_path = 'output/discard/loss_{}_epochs_{}_{}.csv'.format(epochs, date, time)
 
-    # Defining filepath to .h5 files
     path_to_files = '/users/kristofferroise/project/patient_data_proc/'
 
     # Filepath to dataset_information file
     path_to_info = '/users/kristofferroise/project/Diverse/'
     info_filename = 'dataset_information.csv'
+    #===========================================================#
 
-    dataset = Dataset(path_to_info, info_filename)
+    #===================INITIALIZE FILES========================#
+    with open(loss_path, 'w') as els:
+        fieldnames = ['epoch', 'training_loss', 'validation_loss', 'lr=' + str(lr), 'batch_size=' + str(batch_size),
+                      'patch_size=' + str(patch_size), 'stride=' + str(stride),
+                      'number_of_datasets=' + str(tot_num_sets), 'device=' + str(device)]
+        epoch_writer = csv.DictWriter(els, fieldnames=fieldnames)
+        epoch_writer.writeheader()
+    #===========================================================#
+
+    dataset = GetDatasetInformation(path_to_info, info_filename)
 
     fix_set = dataset.fix_files
     mov_set = dataset.mov_files
     fix_vols = dataset.fix_vols
     mov_vols = dataset.mov_vols
 
-    # Shuffling datasets
-    if shuffle_sets:
-        fix_set, mov_set, fix_vols, mov_vols = shuffle(fix_set, mov_set, fix_vols, mov_vols)
-
-    if train:
-        start_time = datetime.now()
-        total_loss = train_network(path_to_files, fix_set, mov_set, epochs, lr, batch_size, patch_size,
-                                   stride, fix_vols, mov_vols, loss_path, use_sets, device, model_name, voxelsize)
-        print('Total time elapsed: ', datetime.now() - start_time)
+    start_time = datetime.now()
+    total_loss = train_network(path_to_files, fix_set, mov_set, epochs, lr, batch_size, patch_size,
+                               stride, fix_vols, mov_vols, loss_path, tot_num_sets, device, model_name, voxelsize, num_validation_sets)
+    print('Total time elapsed: ', datetime.now() - start_time)
