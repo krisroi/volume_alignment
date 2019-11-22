5157fda82e439912c6f3ee74e49c5e306d9fb985
Kristoffer Roise <kristoffer.roise@outlook.com>
2019-11-22 08:00:49 +0100

    Minor change

--- preprocessing/LoadAndVisualize.m
+++ preprocessing/LoadAndVisualize.m
@@ -1,16 +1,16 @@
-path = '/Users/kristofferroise/project/patient_data_raw/';
+path = '/Users/kristofferroise/project/patient_data_proc/';
 group = "DataStOlavs19to28/";
-patient = "p21_3115006/";
+patient = "p22_3115007/";
 
 %load the dataset
-hdfdata1 = HdfImport(strcat(path, group, patient, 'J65BP1I4_ecg.h5'));
-hdfdata2 = HdfImport(strcat(path, group, patient, 'J65BP1I4_proc.h5'));
+hdfdata1 = HdfImport(strcat(path, group, patient, 'J65BP1R0_proc.h5'));
+hdfdata2 = HdfImport(strcat(path, group, patient, 'J65BP1R2_proc.h5'));
 
 
 %sax slice
 for i = 1:3
     slice1 = squeeze(hdfdata1.CartesianVolumes.vol01(:,round(end/2),:));
-    slice2 = squeeze(hdfdata2.CartesianVolumes.vol01(:,round(end/2),:));
+    slice2 = squeeze(hdfdata2.CartesianVolumes.vol12(:,round(end/2),:));
     figure(1); 
     subplot(2,3,1); imshow(slice1, [0 255])
     subplot(2,3,4); imshow(slice2, [0 255])
@@ -19,13 +19,13 @@ end
 
 %lax1
 slice1 = squeeze(hdfdata1.CartesianVolumes.vol01(round(end/2),:,:));
-slice2 = squeeze(hdfdata2.CartesianVolumes.vol01(round(end/2),:,:));
+slice2 = squeeze(hdfdata2.CartesianVolumes.vol12(round(end/2),:,:));
 figure(1), subplot(2,3,2), imshow(slice1, [0 255]);
 figure(1), subplot(2,3,5), imshow(slice2, [0 255]);
 
 %lax2
 slice1 = squeeze(hdfdata1.CartesianVolumes.vol01(:,:,round(end/2)));
-slice2 = squeeze(hdfdata2.CartesianVolumes.vol01(:,:,round(end/2)));
+slice2 = squeeze(hdfdata2.CartesianVolumes.vol12(:,:,round(end/2)));
 figure(1), subplot(2,3,3), imshow(slice1, [0 255]);
 figure(1), subplot(2,3,6), imshow(slice2, [0 255]);
 
