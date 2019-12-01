0198b03d3c99dcab0d0a6b9a78f9822d0e06536f
Kristoffer Roise <kristoffer.roise@outlook.com>
2019-12-01 13:28:17 +0100

    Bug fix

--- predict.py
+++ predict.py
@@ -140,29 +140,27 @@ def predict(path_to_h5files, patch_size, stride, device, voxelsize, model_name, 
 
     print('Predicting')
 
-    predicted_theta_tmp = torch.zeros([len(prediction_loader), int(fixed_patches.shape[0] / batch_size), 12]).type(dtype).to(device)
-    loc_tmp = torch.zeros([len(prediction_loader), int(fixed_patches.shape[0] / batch_size), 3]).type(dtype).to(device)
+    predicted_theta_tmp = torch.zeros([1, batch_size, 12]).type(dtype).to(device)
+    loc_tmp = torch.zeros([1, batch_size, 3]).type(dtype).to(device)
 
     for batch_idx, (fixed_batch, moving_batch, loc) in enumerate(prediction_loader):
 
         printer = progress_printer((batch_idx + 1) / len(prediction_loader))
         print(printer, end='\r')
 
-        print(fixed_batch.is_cuda)
-
         predicted_theta = net(moving_batch)
         predicted_theta = predicted_theta.view(-1, 12)
 
-        predicted_theta_tmp[batch_idx] = predicted_theta.type(dtype)
-        loc_tmp[batch_idx] = loc.type(dtype)
+        predicted_theta_tmp = predicted_theta.type(dtype)
+        loc_tmp = loc.type(dtype)
 
         with open(loc_path, 'a') as lctn:
             lctn_writer = csv.writer(lctn, delimiter=',')
-            lctn_writer.writerows((loc_tmp[batch_idx].cpu().numpy().round(5)))
+            lctn_writer.writerows((loc_tmp.cpu().numpy().round(5)))
 
         with open(theta_path, 'a') as tht:
             theta_writer = csv.writer(tht)
-            theta_writer.writerows((predicted_theta_tmp[batch_idx].cpu().numpy()))
+            theta_writer.writerows((predicted_theta_tmp.cpu().numpy()))
 
     print('Prediction runtime: ', datetime.now() - prediction_start_time)
 
